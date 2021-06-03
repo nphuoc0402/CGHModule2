8927a898f74cd348ab0ff1c61b62fadf848abc95
DELL <nphuoc0402@gmail.com>
2021-06-03 22:05:37 +0700

    adđ project ReverseArray

---
+++ Module2/Week1/ReverseArray/src/ReverseArray.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class ReverseArray {
+    public static void main(String[] args) {
+        int size;
+        int array[];
+        Scanner input = new Scanner(System.in);
+        do{
+            System.out.println("Enter a size: ");
+            size = input.nextInt();
+            if(size>20){
+                System.out.println("Size doesn't exceed 20");
+            }
+        }while(size>20);
+        array = new int[size];
+        for(int i=0;i<array.length;i++){
+            System.out.printf("Enter element %d : ",i+1);
+            array[i] = input.nextInt();
+        }
+        System.out.printf("%-20s%s","Element in array: ","");
+        for(int j=0;j<array.length;j++){
+            System.out.printf("%d\t", array[j]);
+
+        }
+        System.out.println();
+
+        for(int j =0;j<array.length/2;j++){
+            int temp = array[j];
+
+            array[j] = array[array.length-1 - j];
+            array[array.length-j-1] = temp;
+        }
+        System.out.printf("%-20s%s", "Reverse array: ", "");
+        for(int i =0;i<array.length;i++){
+            System.out.printf("%d\t",array[i]);
+        }
+    }
+}
