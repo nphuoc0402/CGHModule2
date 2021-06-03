a8300c06ed83974c7a80004b2a0f13cb40d44cab
DELL <nphuoc0402@gmail.com>
2021-06-03 22:21:00 +0700

    add project FindValueInArray

---
+++ Module2/Week1/FindValueInArray/src/FindValueInArray.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class FindValueInArray {
+    public static void main(String[] args) {
+        String students[] = {"Christian", "Michael", "Camila", "Sienna", "Tanya", "Connor", "Zachariah", "Mallory", "Zoe", "Emily"};
+        Scanner input = new Scanner(System.in);
+        System.out.printf("Enter the name's student:");
+        String name = input.nextLine();
+        boolean check  = false;
+        for(int i = 0; i < students.length;i++){
+            if(students[i].equals(name)){
+                System.out.printf("Position ò the sudents in the list %s is %d",name,i+1);
+                check = true;
+                break;
+            }
+
+
+        }
+        if(!check){
+            System.out.printf("Not found %s in list",name);
+        }
+    }
+}
