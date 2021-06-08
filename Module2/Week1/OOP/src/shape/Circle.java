aca2582ced012e79e4941323b2283b72ad017939
DELL <nphuoc0402@gmail.com>
2021-06-08 21:33:35 +0700

    add package shape

---
+++ Module2/Week1/OOP/src/shape/Circle.java
@@ -0,0 +1,50 @@
+package shape;
+
+public class Circle extends Shape {
+    private double radius = 1.0;
+
+    public Circle(){
+
+    }
+
+    public Circle(double radius){
+        this.radius = radius;
+    }
+
+    public Circle(double radius, String color, boolean filled){
+        super(color,filled);
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public double getArea(){
+        return Math.PI*radius*radius;
+    }
+
+    public double getPerimeter(){
+        return Math.PI*2*radius;
+    }
+    @Override
+    public String toString(){
+        return String.format("A Circle with radius = %f , which is a subclass of %s",getRadius(),super.toString());
+    }
+}
+class CircleTest {
+    public static void main(String[] args) {
+        Circle circle = new Circle();
+        System.out.println(circle);
+
+        circle = new Circle(3.5);
+        System.out.println(circle);
+
+        circle = new Circle(3.5, "indigo", false);
+        System.out.println(circle);
+    }
+}
