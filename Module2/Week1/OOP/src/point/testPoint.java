1adb42fb4be95e97e9a656d861741f25754227a5
DELL <nphuoc0402@gmail.com>
2021-06-09 13:34:13 +0700

    add file

---
+++ Module2/Week1/OOP/src/point/testPoint.java
@@ -0,0 +1,17 @@
+package point;
+
+import java.security.cert.PolicyNode;
+import java.util.Arrays;
+
+public class testPoint {
+    public static void main(String[] args) {
+        Point2D point1 = new Point2D();
+        point1.setXY(2,4);
+        System.out.println(point1.toString());
+        System.out.println(Arrays.toString(point1).getXY());
+        Point2D point2 = new Point3D(2,5,9);
+        point2.setXY(10,9);
+        System.out.println(point2.toString());
+        System.out.println(Arrays.toString(((Point3D) point2).getXYZ()));
+    }
+}
