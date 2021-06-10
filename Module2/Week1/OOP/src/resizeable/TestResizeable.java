91124acf216ab71ce0aa4657fd9a8cc9db413efc
DELL <nphuoc0402@gmail.com>
2021-06-10 20:36:43 +0700

    change TestResizeable.java

--- Module2/Week1/OOP/src/resizeable/TestResizeable.java
+++ Module2/Week1/OOP/src/resizeable/TestResizeable.java
@@ -10,12 +10,14 @@ public class TestResizeable {
         shapes[0] = new Circle(3);
         shapes[1] = new Rectangle(8,12);
         shapes[2] = new Square(5);
-
-
         for(Shape shape : shapes){
             System.out.println(shape);
-            ((Resizeable) shape).resize(50);
-            System.out.println(shape);
+        }
+        ((Resizeable) shapes[0]).resize(50);
+        ((Resizeable) shapes[1]).resize(20);
+        ((Resizeable) shapes[2]).resize(90);
+        for(int i = 0;i<3;i++){
+            System.out.println(shapes[i]);
         }
 
     }
