fa9b2af819d1f545a732092b8f0fa0baa426e8cb
DELL <nphuoc0402@gmail.com>
2021-06-11 08:40:25 +0700

    add projec COuntWordInString

--- Module2/Week1/CountWordInString/src/CountWordInString.java
+++ Module2/Week1/CountWordInString/src/CountWordInString.java
@@ -3,16 +3,19 @@ import java.util.Scanner;
 public class CountWordInString {
     public static void main(String[] args) {
         String s = "";
+        char c;
         int count = 0;
         Scanner input = new Scanner(System.in);
         System.out.println("Enter the String: ");
         s = input.nextLine();
+        System.out.printf("Enter the char c:");
+        c = input.next().charAt(0);
         System.out.printf("%s\n",s);
-        for(int i  = 0;i<s.length();i++){
-            if(s.charAt(i)== 'c'){
+        for(int i  = 0;i<s.length();i++)
+            if (s.charAt(i)==c) {
                 count++;
             }
-        }if(count == 0 ){
+        if(count == 0 ){
             count = -1;
         }
         System.out.printf("The number C found in String is: %d",count);
