bfc0b1267a3a95fa91842c3c0a3d1f0a83de5cd6
DELL <nphuoc0402@gmail.com>
2021-06-14 20:52:15 +0700

    add file ReadNumber

--- Module2/Week1/ReadNumber/src/ReadNumber.java
+++ Module2/Week1/ReadNumber/src/ReadNumber.java
@@ -1,51 +1,34 @@
-import java.util.Scanner;
-
 public class ReadNumber {
+
+    private static final String[] tens = {
+            ""," ten"," twenty"," thirty"," forty"," fifty"," sixty"," seventy"," eighty"," ninety"
+    };
+
+    private static final String[] ones = {
+            ""," one"," two"," three"," four"," five"," six"," seven"," eight"," nine"," ten"," eleven",
+            " twelve"," thirteen"," fourteen"," fifteen"," sixteen"," seventeen"," eighteen"," nineteen"
+    };
+
+    private static String ReadNumberToString(int number) {
+        String result;
+        if (number == 0) { return "zero";}
+        if (number % 100 < 20) {
+            result = ones[number % 100];
+            number /= 100;
+        } else {
+            result = ones[number % 10];
+            number /= 10;
+
+            result = tens[number % 10] + result;
+            number /= 10;
+        }
+        if (number == 0) return result;
+        return ones[number] + " hundred" + result;
+    }
+
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-
-        System.out.println("Enter the number: ");
-        int number = input.nextInt();
-        String message = "";
-        
-            switch (number){
-                case 0:
-
-                    break;
-                case 1:
-                    message = "one";
-                    break;
-                case 2:
-                    message = "two";
-                    break;
-                case 3:
-                    message = "three";
-                    break;
-                case 4:
-                    message = "four";
-                    break;
-                case 5:
-                    message = "five";
-                    break;
-                case 6:
-                    message = "six";
-                    break;
-                case 7:
-                    message = "seven";
-                    break;
-                case 8:
-                    message = "eight";
-                    break;
-                case 9:
-                    message = "nine";
-                    break;
-                default:
-                    message = "Out of Ability";
-                    break;
-            }
-
-
-        System.out.println(message);
-
+        System.out.println(ReadNumber.ReadNumberToString(35));
+        System.out.println(ReadNumber.ReadNumberToString(700));
+        System.out.println(ReadNumber.ReadNumberToString(16));
     }
 }
