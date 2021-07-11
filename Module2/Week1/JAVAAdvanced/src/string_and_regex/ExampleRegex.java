88aeb18466ff00c1498c25c30623ba66a0fe335c
DELL <nphuoc0402@gmail.com>
2021-07-11 16:15:54 +0700

    add

---
+++ Module2/Week1/JAVAAdvanced/src/string_and_regex/ExampleRegex.java
@@ -0,0 +1,19 @@
+package string_and_regex;
+
+public class ExampleRegex {
+
+    static class  A{
+        public static void main(String[] args) {
+            System.out.printf("HelloA");
+        }
+    }
+
+}
+
+
+class B {
+    public static void main(String[] args) {
+        System.out.printf("HelloB");
+    }
+}
+
