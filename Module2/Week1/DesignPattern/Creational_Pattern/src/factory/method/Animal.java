d940faef9c7460993a905b46602e9e2720d7f70f
DELL <nphuoc0402@gmail.com>
2021-07-12 20:45:54 +0700

    add

---
+++ Module2/Week1/DesignPattern/Creational_Pattern/src/factory/method/Animal.java
@@ -0,0 +1,5 @@
+package factory.method;
+
+public abstract class Animal {
+    public abstract  String makeSound();
+}
---
+++ Module2/Week1/DesignPattern/Creational_Pattern/src/factory/method/Cat.java
@@ -0,0 +1,9 @@
+package factory.method;
+
+public class Cat extends Animal{
+
+    @Override
+    public String makeSound() {
+        return "MeoW";
+    }
+}
