6dcb690bc57cfcd81f042dff0b7a95db12f0817f
DELL <nphuoc0402@gmail.com>
2021-07-12 20:46:17 +0700

    add

---
+++ Module2/Week1/DesignPattern/Creational_Pattern/src/objectpool/TaxiNotFoundException.java
@@ -0,0 +1,8 @@
+package objectpool;
+
+public class TaxiNotFoundException extends RuntimeException {
+
+    public TaxiNotFoundException(String message) {
+        System.out.println(message);
+    }
+}
